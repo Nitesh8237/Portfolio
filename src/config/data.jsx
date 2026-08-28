3ce84b628febdb3d6bbe31002615c52c5147c627
Nitesh singh <nsingh.63590@gmail.com>
2026-08-28 17:51:50 +0530

    Initial commit

---
+++ src/config/data.jsx
@@ -0,0 +1,75 @@
+const personalData = {
+    // Home Section
+    name: "NITESH SINGH",
+    title: ["Full Stack Developer", "Web Designer", "Mern Stack Developer"],
+    homeImg: "images/Nitesh.png",
+    email: "nsingh.63590@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/nitesh-singh-743340286/",
+    github: "https://github.com/Nitesh8237",
+    twitter: "https://x.com/Snitesh8237",
+    instagram: "https://www.instagram.com/nitesh_journey/",
+    mail: "mailto:nsingh.63590@gmail.com",
+    resume: "resume/resume.pdf",
+
+    // About Section
+    aboutImg: "images/Niteshabout.png",
+    aboutTitle: "Full Stack Developer",
+    aboutText: "Hi, I'm Nitesh Singh, a frontend developer based in India. I have a passion for web development and I'm currently learning React.js, Node.js,Express,js,MongoDB, HTML, CSS, and JavaScript. I love to create beautiful and functional websites that provide a great user experience. I'm always looking for new challenges and opportunities to learn and grow as a developer.",
+
+    // Services Section
+    service1Title: "Web Development",
+    service1Text: "Experienced in building responsive and user-friendly websites using HTML, CSS, and JavaScript.",
+    service2Title: "Graphic Design",
+    service2Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    service3Title: "Android Development",
+    service3Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+
+    // Education Section
+    edu1Year: "2022 - 2026",
+    edu1Course: "B.Tech",
+    edu1Stream: "Computer Science",
+    edu1College: "SR Institute of Management and Technology",
+    edu1Grade: "A+",
+
+    edu2Year: "2020 - 2022",
+    edu2Course: "Intermediate",
+    edu2Stream: "Science",
+    edu2College: "SR Global School",
+    edu2Grade: "68%",
+
+    edu3Year: "2018 - 2020",
+    edu3Course: "High School",
+    edu3Stream: "Science",
+    edu3College: "Raghukul Academy",
+    edu3Grade: "78%",
+
+  // Portfolio Section
+
+project1Title: "Smart Parking System",
+project1Img: "images/project1.png",
+project1Text: "A smart parking management system developed using Java, Spring Boot, Spring Security, JPA, Hibernate, MySQL, and REST APIs to manage users, parking spaces, and bookings.",
+
+project2Title: "Quirex - Real Estate Portal",
+project2Img: "images/project2.bmp",
+project2Text: "A MERN stack real estate platform that allows users to explore, search, and manage property listings through a responsive and user-friendly interface.",
+
+project3Title: "Personal Portfolio",
+project3Img: "images/project3.webp",
+project3Text: "A responsive personal portfolio website showcasing my skills, projects, education, experience, and contact information using modern web technologies.",
+
+project4Title: "Amazon Clone",
+project4Img: "images/project4.png",
+project4Text: "A responsive e-commerce website inspired by Amazon, built using HTML, CSS, JavaScript, and React.js with a modern and user-friendly shopping interface.",
+
+// project5Title: "BuddyBook - Real-Time Chat Application",
+// project5Img: "images/project5.png",
+// project5Text: "A real-time chatting application built with MongoDB, Express.js, React.js, Node.js, and Socket.io for instant communication between users.",
+
+// project6Title: "Expense Tracker",
+// project6Img: "images/project6.jpg",
+// project6Text: "A web application for recording, managing, and monitoring daily income and expenses with a simple and user-friendly interface.",
+
+
+};
+
+export default personalData;
